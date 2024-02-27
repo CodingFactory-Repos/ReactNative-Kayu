25d918a69e042a012337a0084f479fb16f3842f9
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-02-27 16:25:48 +0100

    Remove jsx and set tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 // import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.screen}>
-      {/*      <NavigationContainer>*/}
-      {/*/!* Add your navigators here *!/*/}
-      {/*      </NavigationContainer>*/}
+      <Text>App</Text>
     </SafeAreaView>
   );
 };
