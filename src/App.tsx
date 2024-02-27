bd8279519eea587dba5dd289c9a328ace65381e1
Louis YANG <louis.yang@animedigitalnetwork.fr>
2024-02-27 15:45:22 +0100

    Adding start of tabbar bottom + navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+// import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.screen}>
-      <Text>Home</Text>
+      {/*      <NavigationContainer>*/}
+      {/*/!* Add your navigators here *!/*/}
+      {/*      </NavigationContainer>*/}
     </SafeAreaView>
   );
 };
