21e533fa7e7278beeb9df6096b77ac79d75caf89
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-02-27 14:05:46 +0100

    Revert "Add  Auth"
    This reverts commit f4fbe8bfdc3b0b001beed9f3248592643aef9d90.

--- App.tsx
+++ App.tsx
@@ -1,46 +1,10 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import {
-  appleAuth,
-  AppleButton,
-} from '@invertase/react-native-apple-authentication';
-
-async function onAppleButtonPress() {
-  // performs login request
-  const appleAuthRequestResponse = await appleAuth.performRequest({
-    requestedOperation: appleAuth.Operation.LOGIN,
-    // Note: it appears putting FULL_NAME first is important, see issue #293
-    requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL],
-  });
-
-  // get current authentication state for user
-  // /!\ This method must be tested on a real device. On the iOS simulator it always throws an error.
-  const credentialState = await appleAuth.getCredentialStateForUser(
-    appleAuthRequestResponse.user,
-  );
-
-  // use credentialState response to ensure the user is authenticated
-  if (credentialState === appleAuth.State.AUTHORIZED) {
-    // user is authenticated
-    console.log('User is authenticated');
-  } else {
-    // user is unauthenticated
-    console.log('User is unauthenticated');
-  }
-}
 
 const App = () => {
   return (
     <SafeAreaView style={styles.screen}>
-      <AppleButton
-        buttonStyle={AppleButton.Style.WHITE}
-        buttonType={AppleButton.Type.SIGN_IN}
-        style={{
-          width: 160, // You must specify a width
-          height: 45, // You must specify a height
-        }}
-        onPress={() => onAppleButtonPress()}
-      />
+      {/*Create login screen here*/}
     </SafeAreaView>
   );
 };
