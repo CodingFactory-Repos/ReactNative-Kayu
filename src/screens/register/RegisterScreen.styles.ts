2971a21d70024c24df9fe6e64d0308ec9c4be4e6
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-02-28 16:54:20 +0100

    Add register

---
+++ src/screens/register/RegisterScreen.styles.ts
@@ -0,0 +1,70 @@
+import {StyleSheet} from 'react-native';
+import {Colors} from '../../utils/colors.ts';
+
+const baseTextInput = {
+  borderWidth: 1,
+  padding: 10,
+  borderRadius: 10,
+  borderColor: 'gray',
+};
+
+export const styles = StyleSheet.create({
+  safeAreaView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  view: {
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    backgroundColor: Colors.primary,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    marginTop: 20,
+  },
+  innerView: {
+    width: '80%',
+  },
+  textInput: {
+    ...baseTextInput,
+  },
+  passwordInput: {
+    ...baseTextInput,
+    marginTop: 10,
+    color: 'black',
+  },
+  buttonView: {
+    marginTop: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  leftText: {
+    textAlign: 'left',
+    width: '50%',
+  },
+  rightText: {
+    textAlign: 'right',
+    width: '100%',
+  },
+  button: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 50,
+    alignItems: 'center',
+    width: '80%',
+  },
+  buttonText: {
+    fontSize: 18,
+  },
+});
--- src/screens/register/RegisterScreen.tsx
+++ src/screens/register/RegisterScreen.tsx
@@ -1,12 +1,59 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {
+  Image,
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {styles} from './RegisterScreen.styles.ts';
+import {useNavigation} from '@react-navigation/native';
+import {ACCOUNT_NAVIGATOR_ROUTES} from '../../components/navigators/AccountNavigator/AccountNavigator.interfaces.ts';
 
 const RegisterScreen = () => {
+  const navigation = useNavigation();
+
+  function RedirectToLogin() {
+    navigation.navigate(ACCOUNT_NAVIGATOR_ROUTES.LOGIN);
+  }
+
   return (
-    <SafeAreaView>
-      <Text>RegisterScreen</Text>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={styles.safeAreaView}>
+        <Image
+          source={require('../../../assets/kayu.png')}
+          style={styles.image}
+        />
+        <Text style={styles.text}>Welcome to Kayu</Text>
+      </SafeAreaView>
+      <View style={styles.view}>
+        <View style={styles.innerView}>
+          <View>
+            <TextInput
+              placeholder="Email"
+              textContentType={'emailAddress'}
+              style={styles.textInput}
+            />
+            <TextInput
+              placeholder="Password"
+              textContentType={'password'}
+              style={styles.passwordInput}
+            />
+          </View>
+          <View style={styles.buttonView}>
+            <View style={styles.row}>
+              <Text style={styles.rightText} onPress={() => RedirectToLogin()}>
+                Already have an account?
+              </Text>
+            </View>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
-
 export default RegisterScreen;
