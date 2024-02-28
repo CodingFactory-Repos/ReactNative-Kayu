10083e45375deed211836c8b3ec55c8fef6d89c8
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-02-28 12:02:09 +0100

    Add logo

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 const LoginScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>LoginScreen</Text>
+    <SafeAreaView style={styles.screen}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Image
+          source={require('../../../assets/kayu.png')}
+          style={{width: 200, height: 200}}
+        />
+      </View>
+      <View style={{flex: 3}}>
+        <Text>Form</Text>
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
 
 export default LoginScreen;
