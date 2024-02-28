66b1893a0fe6e2cdc29714c2a825c13f18f9eb49
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-02-28 19:23:25 +0100

    Update LoginScreen.tsx

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -27,11 +27,12 @@ const LoginScreen = () => {
     navigation.navigate(ACCOUNT_NAVIGATOR_ROUTES.REGISTER);
   }
 
-  function validateEmail(email) {
+  function validateEmail(email: string): boolean {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   }
-  function validatePassword(password) {
+
+  function validatePassword(password: string): boolean {
     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
     return passwordRegex.test(password);
   }
