3dea292242b298845d19b41f2a2a09e267ee04da
Louis YANG <louis.yang@animedigitalnetwork.fr>
2024-02-28 16:38:40 +0100

    Pushing the hiding headers when we are in login page

---
+++ src/components/navigators/TabBarNavigation/TabNavigator.interfaces.ts
@@ -0,0 +1,15 @@
+export enum TAB_BAR_NAVIGATOR_ROUTES {
+  ACCOUNT = 'ACCOUNT',
+  CARROT = 'CARROT',
+  SEARCH = 'SEARCH',
+  QRSCAN = 'QRSCAN',
+  PLATE = 'PLATE',
+}
+
+export type TabBarNavigatorParamList = {
+  [TAB_BAR_NAVIGATOR_ROUTES.ACCOUNT]: undefined;
+  [TAB_BAR_NAVIGATOR_ROUTES.CARROT]: undefined;
+  [TAB_BAR_NAVIGATOR_ROUTES.SEARCH]: undefined;
+  [TAB_BAR_NAVIGATOR_ROUTES.QRSCAN]: undefined;
+  [TAB_BAR_NAVIGATOR_ROUTES.PLATE]: undefined;
+};
