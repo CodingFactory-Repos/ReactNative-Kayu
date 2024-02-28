f98c9ed1c02f8170f1e2743ed96ba16228ab08f8
Ruiseki <jojobru60@gmail.com>
2024-02-28 16:30:37 +0100

    imager + service -> product scan

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,22 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {useCameraPermission} from 'react-native-vision-camera';
+import { Imager } from './screens/imager/Imager';
 
 const Tab = createBottomTabNavigator();
 
 const LastScanScreen = () => null;
 const PlateScreen = () => null;
-const QRScanScreen = () => null;
+const QRScanScreen = () => Imager();
 const SearchScreen = () => null;
 const AccountScreen = () => null;
 
 const App = () => {
-  const {hasPermission, requestPermission} = useCameraPermission();
   const renderIcon =
     name =>
     ({color, size}) =>
       <Icon name={name} color={color} size={size} />;
-
-  useEffect(() => {
-    if (!hasPermission) {
-      requestPermission().then(r => console.log('requested permission', r));
-    }
-  }, [hasPermission, requestPermission]);
 
   return (
     <NavigationContainer>
