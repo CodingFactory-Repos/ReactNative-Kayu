2816e0f22fd9f0d526150a2aa76865a6c3eb9909
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-02-29 22:19:24 +0100

    Update TabNavigator.tsx

--- src/components/navigators/TabBarNavigation/TabNavigator.tsx
+++ src/components/navigators/TabBarNavigation/TabNavigator.tsx
@@ -10,6 +10,7 @@ import {
   TabBarNavigatorParamList,
 } from './TabNavigator.interfaces.ts';
 import {Imager} from '../../../screens/imager/Imager';
+import {Image, Text, View} from 'react-native';
 
 const Tab = createBottomTabNavigator<TabBarNavigatorParamList>();
 
@@ -36,7 +37,34 @@ const TabNavigator = () => {
     <Tab.Navigator
       initialRouteName={TAB_BAR_NAVIGATOR_ROUTES.CARROT}
       screenOptions={{
-        headerShown: false,
+        headerShown: true,
+        // Set logo in left side of header
+        headerLeft: () => (
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Image
+              source={require('../../../../assets/kayu.png')}
+              style={{width: 40, height: 40}}
+            />
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: 'bold',
+                color: 'black',
+              }}>
+              Kayu
+            </Text>
+          </View>
+        ),
+        headerRight: () => (
+          <Icon
+            name="camera"
+            size={25}
+            color="black"
+            style={{marginRight: 10}}
+            onPress={() => navigate(TAB_BAR_NAVIGATOR_ROUTES.QRSCAN)}
+          />
+        ),
+        headerTitle: '',
       }}>
       <Tab.Screen
         name={TAB_BAR_NAVIGATOR_ROUTES.CARROT}
