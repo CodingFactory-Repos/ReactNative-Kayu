1b1cc95db9662a520c2278ddb09ed1455b1235bc
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-03-01 10:17:04 +0100

    Create modal for adding plate

--- src/screens/plate/PlateScreen.tsx
+++ src/screens/plate/PlateScreen.tsx
@@ -1,16 +1,40 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 
-import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
+import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
 
 const PlateScreen = () => {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const [isBottomSheetModalVisible, setBottomSheetModalVisible] =
+    useState(false);
   useEffect(() => {}, []);
 
+  function openModal() {
+    console.log('open modal');
+    setBottomSheetModalVisible(true);
+  }
+
+  const handleSheetChanges = useCallback((index: number) => {
+    if (index === -1) setBottomSheetModalVisible(false);
+  }, []);
+
   return (
-    <SafeAreaView>
-      <TouchableOpacity>
+    <View style={{flex: 1}}>
+      <TouchableOpacity onPress={() => openModal()}>
         <Text>Open modal</Text>
       </TouchableOpacity>
-    </SafeAreaView>
+      {isBottomSheetModalVisible && (
+        <BottomSheet
+          ref={bottomSheetRef}
+          onChange={handleSheetChanges}
+          snapPoints={['20%', '100%']}
+          enablePanDownToClose={true}>
+          <BottomSheetView>
+            <Text>Swipe down to close</Text>
+          </BottomSheetView>
+        </BottomSheet>
+      )}
+    </View>
   );
 };
 
